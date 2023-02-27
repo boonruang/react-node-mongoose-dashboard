3fc7082fa1c9e3e43de19df273d02bf5c348b0cf
boonruang <scubatoy@gmail.com>
2023-02-27 15:09:08 +0700

    done setup sidebar and navbar

--- client/src/scene/layout/index.jsx
+++ client/src/scene/layout/index.jsx
@@ -1,31 +1,37 @@
-import React, { useState } from 'react'
-import { Box, useMediaQuery } from '@mui/material'
-import { Outlet } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import Navbar from 'components/Navbar'
-import Sidebar from 'components/Sidebar'
+import React, { useState } from 'react';
+import { Box, useMediaQuery } from '@mui/material';
+import { Outlet } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import Navbar from 'components/Navbar';
+import Sidebar from 'components/Sidebar';
+import { useGetUserQuery } from 'state/api';
 
 const Layout = () => {
-    const isNonMobile = useMediaQuery('(min-width: 600px)')
-    const [isSidebarOpen, setIssidebarOpen] = useState(true)
-    return (
-        <Box display={isSidebarOpen ? "flex" : "block"} width="100%" height="100%">
-            <Sidebar
-                isNonMobile={isNonMobile}
-                drawerWidth='250px'
-                isSidebarOpen={isSidebarOpen}
-                setIssidebarOpen={setIssidebarOpen}
-            />
+  const isNonMobile = useMediaQuery('(min-width: 600px)');
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const userId = useSelector((state) => state.global.userId);
+  const { data } = useGetUserQuery(userId);
 
-            <Box >
-                <Navbar
-                    isSidebarOpen={isSidebarOpen}
-                    setIssidebarOpen={setIssidebarOpen}
-                />
-                <Outlet />
-            </Box>
-        </Box >
-    )
-}
+  return (
+    <Box display={isSidebarOpen ? 'flex' : 'block'} width='100%' height='100%'>
+      <Sidebar
+        user={data || {}}
+        isNonMobile={isNonMobile}
+        drawerWidth='250px'
+        isSidebarOpen={isSidebarOpen}
+        setIsSidebarOpen={setIsSidebarOpen}
+      />
 
-export default Layout
+      <Box flexGrow={1}>
+        <Navbar
+          user={data || {}}
+          isSidebarOpen={isSidebarOpen}
+          setIsSidebarOpen={setIsSidebarOpen}
+        />
+        <Outlet />
+      </Box>
+    </Box>
+  );
+};
+
+export default Layout;
--- server/index.js
+++ server/index.js
@@ -11,6 +11,10 @@ import generalRoutes from './routes/general.js'
 import managementRoutes from './routes/management.js'
 import salesRoutes from './routes/sales.js'
 
+// Data imports
+import User from './models/User.js'
+import { dataUser } from './data/index.js'
+
 // CONFIGURATION
 
 dotenv.config()
@@ -37,6 +41,9 @@ mongoose.connect(process.env.MONGO_URL, {
     useUnifiedTopology: true,
 }).then(() => {
     app.listen(PORT, () => console.log(`Server port: ${PORT}`))
+
+    // ONLY ADD DATA ONE TIME
+    // User.insertMany(dataUser)
 }).catch((error) => console.log(`${error} did not connect`))
 
 mongoose.set('strictQuery', true)
